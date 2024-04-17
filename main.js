02386c78c2f9dc05780bf75ce0f42c79d6b03633
Santinop145 <santinoalessandrop@gmail.com>
2024-04-17 17:56:38 -0300

    actualiza la pagina web

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ function handleSubmit(event) {
     event.preventDefault();
     let n = +document.forms[0].n.value;
     let h3 = document.querySelector('#bin');
+    let hex = document.querySelector('#hex');
     h3.textContent = n.toString(2);
+    hex.textContent = n.toString(16);
   }
